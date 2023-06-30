a76af2a56f0d502888fd3e8fad7de671f8a7c28c
Leo Garbe <Leoko4433@gmail.com>
2023-06-30 14:50:12 +0200

    Moved GettingStartedSection into own component

--- frontend/src/lib/components/getting-started/GettingStartedSection.svelte
+++ frontend/src/lib/components/getting-started/GettingStartedSection.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
 	import { createUserSettingsQuery } from '../../controller/user-settings'
+	import Button from '../basics/Button.svelte'
+	import Modal from '../basics/Modal.svelte'
+	import GettingStartedView from './GettingStartedView.svelte'
 
 	const userSettings = createUserSettingsQuery()
+
+	const firstTimeGettingStarted = localStorage.getItem('firstTimeGettingStarted') != 'false'
 
 	let specified = {
 		businessName: false,
@@ -24,21 +29,28 @@
 			}
 		}
 	}
+
+	let gettingStartedModalOpen = firstTimeGettingStarted
+
+	function closeModal() {
+		gettingStartedModalOpen = false
+		localStorage.setItem('firstTimeGettingStarted', 'false')
+	}
 </script>
 
-<h2 class="pageSubTitle">Welcome to Liberty Invoice!</h2>
+<h2 class="flex items-center pageSubTitle">Welcome to Liberty Invoice!</h2>
 
 <div class="text-gray-500">
-	We're glad you're here. To use the full potential of Liberty Invoice you should specify:
+	To use the full potential of Liberty Invoice you should specify:
 
-	<div class="flex flex-col">
+	<div class="flex flex-col my-2">
 		{#each specifiedKeys as key}
 			<div class="flex items-center {!specified[key] ? 'text-gray-500' : ' text-lime-500'}">
-				<span class="mr-1 text-sm material-icons-outlined">
+				<span class="mr-2 text-sm material-icons-outlined">
 					{#if specified[key]}
 						check
 					{:else}
-						circle
+						remove
 					{/if}
 				</span>
 				{#if key == 'businessName'}
@@ -52,5 +64,13 @@
 		{/each}
 	</div>
 
-	<Button>Get started</Button>
+	<Button class="mt-3" outlined on:click={() => (gettingStartedModalOpen = true)}
+		>Get started</Button
+	>
 </div>
+
+{#if gettingStartedModalOpen}
+	<Modal>
+		<GettingStartedView on:close={closeModal} />
+	</Modal>
+{/if}
