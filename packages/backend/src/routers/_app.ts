57b2f351bf16d23303452a0a86aef29f33f80fb0
Leo Garbe <Leoko4433@gmail.com>
2024-03-01 22:07:51 +0100

    Undid debugging code

--- packages/backend/src/routers/_app.ts
+++ packages/backend/src/routers/_app.ts
@@ -1,15 +1,23 @@
 import type { inferRouterInputs, inferRouterOutputs } from '@trpc/server'
 import { router } from '../trpc'
+import { authRouter } from './auth'
+import { clientRouter } from './client'
+import { invoiceRouter } from './invoice'
+import { productRouter } from './product'
+import { statsRouter } from './stats'
+import { taxRateRouter } from './tax-rate'
+import { textFragmentRouter } from './text-fragment'
+import { userSettingsRouter } from './user-settings'
 
 export const appRouter = router({
-	// auth: authRouter,
-	// client: clientRouter,
-	// userSettings: userSettingsRouter,
-	// invoice: invoiceRouter,
-	// textFragment: textFragmentRouter,
-	// taxRate: taxRateRouter,
-	// stats: statsRouter,
-	// product: productRouter,
+	auth: authRouter,
+	client: clientRouter,
+	userSettings: userSettingsRouter,
+	invoice: invoiceRouter,
+	textFragment: textFragmentRouter,
+	taxRate: taxRateRouter,
+	stats: statsRouter,
+	product: productRouter,
 })
 
 export type AppRouter = typeof appRouter
